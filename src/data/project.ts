0b832298b64991b25f1a3688dc4a0236eb230c8d
sithuaunglife <johnsta27@gmail.com>
2026-04-21 15:52:06 +0630

    chore: add and edit projects and change photo

--- src/data/project.ts
+++ src/data/project.ts
@@ -1,11 +1,22 @@
 export const project = [
   {
     id: "1",
+    title: "Zhangliang Mala POS",
+    description:
+      "Mala POS System – Frontend Developer (Next.js, Team Collaboration)",
+    image: "/zhangliang-mala-pos.png",
+    techStack: ["Next.js", "Tailwind", "React", "TypeScript"],
+    githubLink: "#https://github.com/heinhtetzan/zhangliang-mala-pos.git",
+    liveLink: "https://zhangliang-mala-pos.vercel.app/",
+  },
+  {
+    id: "2",
     title: "To-do list",
-    description: "My first personal to-do list project using Next.js and react.",
+    description:
+      "My first personal to-do list project using Next.js and react.",
     image: "/to-doList.png",
-    techStack: ["Next.js", "Tailwind"],
+    techStack: ["Next.js", "Tailwind", "React"],
     githubLink: "#https://github.com/sithuaunglife/todo-list.git",
-    liveLink: "#",
-  }
+    liveLink: "https://todo-list-xi-three-21.vercel.app",
+  },
 ];
